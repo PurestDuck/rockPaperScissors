8954c82774c2b8ddf842bc48a07cdd8904ff62a6
Diego <pureduck101@gmail.com>
2022-02-26 17:26:33 -0800

    Add declareWinner

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-function computerPlay(){
-    let moveNumber = Math.floor(Math.random()*3)+1;
+function computerPlay() {
+    let moveNumber = Math.floor(Math.random() * 3) + 1;
     let computerChoice;
-    if(moveNumber===1){
+    if (moveNumber === 1) {
         computerChoice = "rock";
-    }else if(moveNumber===2){
+    } else if (moveNumber === 2) {
         computerChoice = "scissors";
-    }else{
+    } else {
         computerChoice = "paper";
     }
     return computerChoice;
 }
-console.log(computerPlay());
+function declareWinner(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection) {
+        case ("rock"):
+            console.log(computerSelection);
+            if (computerSelection === "rock") {
+                return "Tie!";
+            } else if (computerSelection === "scissors") {
+                return "Winner!";
+            } else {
+                return "Lose!";
+            }
+        case ("scissors"):
+            console.log(computerSelection);
+            if (computerSelection === "scissors") {
+                return "Tie!";
+            } else if (computerSelection === "paper") {
+                return "Winner!";
+            } else {
+                return "Lose!";
+            }
+        case ("paper"):
+            console.log(computerSelection);
+            if (computerSelection === "paper") {
+                return "Tie!";
+            } else if (computerSelection === "rock") {
+                return "Winner!";
+            } else {
+                return "Lose!";
+            }
+    }
+}
+
+console.log(declareWinner("roCk", computerPlay()));
+
