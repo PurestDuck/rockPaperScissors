d7d76a1f029213295061cc518e0f6f2f0077b5a6
Diego <pureduck101@gmail.com>
2022-02-26 17:11:24 -0800

    Fix computerPlay

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function computerPlay(){
     let moveNumber = Math.floor(Math.random()*3)+1;
-    return moveNumber;
+    let computerChoice;
+    if(moveNumber===1){
+        computerChoice = "rock";
+    }else if(moveNumber===2){
+        computerChoice = "scissors";
+    }else{
+        computerChoice = "paper";
+    }
+    return computerChoice;
 }
 console.log(computerPlay());
